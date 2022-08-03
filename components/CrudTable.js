23145b902caae11b2df07d963eba4e5454571419
Aaki <aakarshit.vohra@codashs.com>
2022-08-03 14:00:43 +0530

    dataTable fixed and value column added

--- components/CrudTable.js
+++ components/CrudTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
@@ -14,39 +14,16 @@ import { Card } from 'primereact/card';
 function CrudTable(props) {
 
     // empty Invoice
-    let emptyInvoice = {
-        "invoice-id": "",
-        "fr-recn": "",
-        "customer-id": "",
-        "sac": [
-            {
-                "998313": "Consulting and support services(Export Services)"
-            }
-        ],
-        "invoice-type": "Export",
-        "invoice-date": "",
-        "supply-place": "",
-        "invoice-details": [
-            {
+    let emptyInvoice = 
+            {   "id":"",
                 "sr-no": "",
                 "description": "",
                 "quantity": "",
                 "rate": "",
-                "currency": "",
+                "currency": 0,
                 "value": ""
             }
-        ],
-        "total-value": "110",
-        "discount": "",
-        "cgst": "",
-        "sgst": "",
-        "igst": "",
-        "ugst": "",
-        "cess": "",
-        "total-tax": "",
-        "invoice-value-str": "one hundred ten only"
-    }
-
+    
 
     // states
     const [invoices, setInvoices] = useState([]);
@@ -60,43 +37,6 @@ function CrudTable(props) {
     const [customerSelected, setCustomerSelected] = useState(null)
     const toast = useRef(null);
     const dt = useRef(null);
-
-    useEffect(() => {
-        setInvoices([
-            {
-                "invoice-id": "",
-                "fr-recn": "",
-                "customer-id": "",
-                "sac": [
-                    {
-                        "998313": "Consulting and support services(Export Services)"
-                    }
-                ],
-                "invoice-type": "Export",
-                "invoice-date": "",
-                "supply-place": "",
-                "invoice-details": [
-                    {
-                        "sr-no": "",
-                        "description": "hello",
-                        "quantity": "8",
-                        "rate": "9",
-                        "currency": "",
-                        "value": ""
-                    }
-                ],
-                "total-value": "110",
-                "discount": "",
-                "cgst": "",
-                "sgst": "",
-                "igst": "",
-                "ugst": "",
-                "cess": "",
-                "total-tax": "",
-                "invoice-value-str": "one hundred ten only"
-            }
-        ]);
-    }, []);
 
     // functions
     const openNew = () => {
@@ -122,24 +62,15 @@ function CrudTable(props) {
 
     const saveInvoice = () => {
         setSubmitted(true);
-        // here is  a problem  that "name" is used
-        // if (invoice.name.trim()) {
         let _invoice = { ...invoice };
         let _invoices = [...invoices ];
-        //this for upadting invices
-        if (invoice["invoice-id"]) {
-            const index = findIndexById(invoice["invoice-id"])
-            _invoices[index] = _invoice;  //here invoice gets inside invoices
-            //delete below
-            // fetch('/api/invoiceCRUD', {
-            //     method: 'PUT',
-            //     body: { invoice: JSON.stringify(_invoice), index: index }
-            // })
+        if (invoice.id) {
+            const index = findIndexById(_invoice.id)
+            _invoices[index] = _invoice;
             toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Invoice Updated', life: 3000 });
         }
         else {
-                _invoice.id = createId();
-                _invoice.image = 'invoice-placeholder.svg';
+            _invoice.id = createId();
             _invoices.push(_invoice);
             toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Invoice Created', life: 3000 });
         }
@@ -148,12 +79,10 @@ function CrudTable(props) {
         setInvoices(_invoices)
         setInvoiceDialog(false);
         setInvoice(emptyInvoice);
-        // }
     }
 
 
 
-    // more functions
     const editInvoice = (invoice) => {
         setInvoice({ ...invoice });
         setInvoiceDialog(true);
@@ -167,11 +96,6 @@ function CrudTable(props) {
     const deleteInvoice = () => {
         let _invoices = invoices.filter(val => val.id !== invoice.id);
         setInvoices(_invoices);
-        // delete below
-        // fetch('/api/invoiceCRUD', {
-        //     method: 'DELETE',
-        //     body: JSON.stringify([invoice])
-        // })
         setDeleteInvoiceDialog(false);
         setInvoice(emptyInvoice);
         toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Invoice Deleted', life: 3000 });
@@ -180,13 +104,14 @@ function CrudTable(props) {
     const findIndexById = (id) => {
         let index = -1;
         for (let i = 0; i < invoices.length; i++) {
-            if (invoices[i]["invoice-id"] === id) {
+            if (invoices[i].id === id) {
                 index = i;
                 break;
             }
         }
         return index;
     }
+ 
     const createId = () => {
         let id = '';
         let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -197,7 +122,7 @@ function CrudTable(props) {
     }
 
 
-    // selected invoices only can be deleted Together and these are the functions for it
+    // selected invoices only; can be deleted Together and these are the functions for it
     const confirmDeleteSelected = () => {
         setDeleteInvoicesDialog(true);
     }
@@ -205,12 +130,6 @@ function CrudTable(props) {
     const deleteSelectedInvoices = () => {
         let _invoices = invoices.filter(val => !selectedInvoices.includes(val));
         setInvoices(_invoices);
-      //  delete below
-        // fetch('/api/invoiceCRUD', {
-        //     method: 'DELETE',
-        //     body: JSON.stringify(selectedInvoices)
-        // })
-
         setDeleteInvoicesDialog(false);
         setSelectedInvoices(null);
         toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Invoices Deleted', life: 3000 });
@@ -219,15 +138,16 @@ function CrudTable(props) {
     const onInputChange = (e, name) => {
         const val = (e.target && e.target.value) || '';
         let _invoice = { ...invoice };
-        _invoice["invoice-details"][0][`${name}`] = val;
-
+        _invoice[`${name}`] = val;
         setInvoice(_invoice);
     }
 
     const onInputNumberChange = (e, name) => {
         const val = e.value || 0;
         let _invoice = { ...invoice };
-        _invoice["invoice-details"][0][`${name}`] = val;
+        _invoice[`${name}`] = val;
+        _invoice.value = _invoice.quantity*_invoice.rate;
+
 
         setInvoice(_invoice);
     }
@@ -298,11 +218,12 @@ function CrudTable(props) {
 
     // column map
     const columns = [
-        { field: "invoices-details"[0].description, header: "Description" },
-        { field: "invoices-details"[0].currency, header: "Currency" },
-        { field: "invoices-details"[0].quantity, header: "Quantity" },
-        { field: "invoices-details"[0].rate, header: "Rate" },
-        { field: "invoices-details"[0].value, header: "Value" },
+        { field: "sr-no", header: "Serial Number" },
+        { field: "description", header: "Description" },
+        { field: "quantity", header: "Quantity" },
+        { field: "currency", header: "Currency" },
+        { field: "rate", header: "Rate" },
+        { field: "value", header: "Value" },
     ];
 
     const dynamicColumns = columns.map((col, i) => {
@@ -323,9 +244,7 @@ function CrudTable(props) {
                     <Toolbar className="mb-4" left={leftToolbarTemplate}></Toolbar>
 
                     <DataTable ref={dt} value={invoices} selection={selectedInvoices} onSelectionChange={(e) => { setSelectedInvoices(e.value); console.log(e, "eee") }}
-                        dataKey="invoice-id" paginator rows={10} rowsPerPageOptions={[5, 10, 25]}
-                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-                        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} invoices"
+                        dataKey="id"
                         globalFilter={globalFilter} header={header} responsiveLayout="scroll">
                         <Column selectionMode="multiple" headerStyle={{ width: '3rem' }} exportable={false}></Column>
                         {dynamicColumns}
@@ -336,21 +255,22 @@ function CrudTable(props) {
 
 
                 <Dialog visible={invoiceDialog} style={{ width: '450px' }} header="Invoice Details" modal className="p-fluid" footer={invoiceDialogFooter} onHide={hideDialog}>
-                    {/* {product.image && <img src={`images/product/${product.image}`} onError={(e) => e.target.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={product.image} className="product-image block m-auto pb-3" />} */}
-
+                    <div className="field col">
+                        <label htmlFor="sr-no">Serial Number</label>
+                        <InputNumber id="sr-no" value={invoice["sr-no"]} onChange={(e) => onInputNumberChange(e, 'sr-no')} required rows={3} cols={20} />
+                    </div>
                     <div className="field">
                         <label htmlFor="description">Description</label>
-                        <InputTextarea id="description" value={invoice["invoice-details"][0].description} onChange={(e) => onInputChange(e, 'description')} required rows={3} cols={20} />
+                        <InputTextarea id="description" value={invoice.description} onChange={(e) => onInputChange(e, 'description')} required rows={3} cols={20} />
                     </div>
-
                     <div className="formgrid grid">
                         <div className="field col">
                             <label htmlFor="rate">Rate</label>
-                            <InputNumber id="rate" value={invoice["invoice-details"][0].rate} onValueChange={(e) => onInputNumberChange(e, 'rate')} mode="currency" currency="USD" locale="en-US" />
+                            <InputNumber id="rate" value={invoice.rate} onValueChange={(e) => onInputNumberChange(e, 'rate')} mode="currency" currency="INR" locale="en-IN" />
                         </div>
                         <div className="field col">
                             <label htmlFor="quantity">Quantity</label>
-                            <InputNumber id="quantity" value={invoice["invoice-details"][0].quantity} onValueChange={(e) => onInputNumberChange(e, 'quantity')} integeronly />
+                            <InputNumber id="quantity" value={invoice.quantity} onValueChange={(e) => onInputNumberChange(e, 'quantity')} integeronly />
                         </div>
                     </div>
                 </Dialog>
@@ -358,7 +278,7 @@ function CrudTable(props) {
                 <Dialog visible={deleteInvoiceDialog} style={{ width: '450px' }} header="Confirm" modal footer={deleteInvoiceDialogFooter} onHide={hideDeleteInvoiceDialog}>
                     <div className="confirmation-content">
                         <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
-                        {invoice && <span>Are you sure you want to delete <b>{invoice['invoice-id']}</b>?</span>}
+                        {invoice && <span>Are you sure you want to delete invoice number<b>{invoice['sr-no']}</b>?</span>}
                     </div>
                 </Dialog>
 
